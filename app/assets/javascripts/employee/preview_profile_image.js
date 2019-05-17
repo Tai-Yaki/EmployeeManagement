ab60647f622d38b3a507ebfb37e142af9adbfbde
Taiki Yamada <t.yamada@chapter-5.com>
2019-05-17 14:26:57 +0900

    Can setting profile image

--- app/assets/javascripts/employee/preview_profile_image.js
+++ app/assets/javascripts/employee/preview_profile_image.js
@@ -1,29 +1,31 @@
-$(function() {
-  $("form").on("change", 'input[type="file"]', function(e) {
-    var file = e.target.files[0],
-        reader = new FileReader(),
-        $preview = $(".image-preview");
-
-    if(file.type.indexOf("image") < 0) {
-      return false;
-    }
-
-    reader.onload = (function(file) {
-      return function(e) {
-        $preview.empty();
-        $preview.append($("<img>").attr({
-          src: e.target.result,
-          class: "preview",
-          title: file.name
-        }));
-
-        $(".preview").css("width", "128px");
-        $(".preview").css("height", "128px");
-        $(".preview").css("object-fit", "contain");
-        $(".preview").css("background-color", "black");
-      };
-    })(file);
-
-    reader.readAsDataURL(file);
+$(document).on("ready page:load", function() {
+  $(function() {
+    $("form").on("change", 'input[type="file"]', function(e) {
+      var file = e.target.files[0],
+          reader = new FileReader(),
+          $preview = $(".image-preview");
+  
+      if(file.type.indexOf("image") < 0) {
+        return false;
+      }
+  
+      reader.onload = (function(file) {
+        return function(e) {
+          $preview.empty();
+          $preview.append($("<img>").attr({
+            src: e.target.result,
+            class: "preview",
+            title: file.name
+          }));
+  
+          $(".preview").css("width", "128px");
+          $(".preview").css("height", "128px");
+          $(".preview").css("object-fit", "contain");
+          $(".preview").css("background-color", "black");
+        };
+      })(file);
+  
+      reader.readAsDataURL(file);
+    });
   });
 })
--- app/assets/javascripts/employee/set_profile_image.js
+++ app/assets/javascripts/employee/set_profile_image.js
@@ -1,14 +1,16 @@
-$(function() {
-  $(".image-setter").click(function() {
-    $("#prof-image").click();
-  });
+$(document).on("turbolinks:load", function() {
+  $(function() {
+    $(".image-setter").click(function() {
+      $("#prof-image").click();
+    });
 
-  $(".image-setter").hover(function() {
-    $(".image-setter").css("background-color", "rgba(0, 0, 0, 0.5)");
-    $(".image-camera").css("opacity", "1");
-  }, 
-  function() {
-    $(".image-setter").css("background-color", "transparent");
-    $(".image-camera").css("opacity", "0.5");
+    $(".image-setter").hover(function() {
+      $(".image-setter").css("background-color", "rgba(0, 0, 0, 0.5)");
+      $(".image-camera").css("opacity", "1");
+    }, 
+    function() {
+      $(".image-setter").css("background-color", "transparent");
+      $(".image-camera").css("opacity", "0.5");
+    })
   })
 })
