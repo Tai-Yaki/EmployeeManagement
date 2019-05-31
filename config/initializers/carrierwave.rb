5ccdd0ccaabfae5376e41162ececb9e1907ab45d
Taiki Yamada <t.yamada@chapter-5.com>
2019-05-31 10:58:41 +0900

    Fix ENV_key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV[S3_RAILS_PROFILE_PHOTO_ACCESS_KEY],
-      aws_secret_access_key: ENV[S3_RAILS_PROFILE_PHOTO_SECRET_ACCESS_KEY],
+      aws_access_key_id: ENV['S3_RAILS_PROFILE_PHOTO_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_RAILS_PROFILE_PHOTO_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1'
     }
   
